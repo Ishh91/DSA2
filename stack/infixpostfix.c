91377aa9a68c754f84f7e635d8d2e91750897abc
Ishh91 <ishantsharma9105@gmail.com>
2022-10-16 21:19:14 +0530

    intopost

---
+++ stack/infixpostfix.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+struct Node
+{
+    char data;
+    struct Node *next;
+} *top = NULL;
+
+void push(char x)
+{
+    struct Node *t;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    if (t == NULL)
+    {
+        printf("stack is full\n");
+    }
+    else
+    {
+        t->data = x;
+        t->next = top;
+        top = t;
+    }
+}
+char pop()
+{
+    struct Node *p;
+    char x = -1;
+    if (top == NULL)
+    {
+        printf("Stack is Empty:");
+    }
+    else
+    {
+        p = top;
+        top = top->next;
+        x = p->data;
+        free(p);
+    }
+    printf("\n");
+    return x;
+}
+void Display()
+{
+    struct Node *p = top;
+    while (p != NULL)
+    {
+        printf("%d", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+
+int pre(char x){
+    if(x == '+' || x == '-'){
+        return 1;
+    }
+    else if (x == '*' || x == '/')
+    {
+        return 2;
+        /* code */
+    }
+    return 0;
+    
+}
+int isoperand(char x){
+    if(x == '+' || x == '-' || x == '*' || x == '/'){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+char *Convert(char *infix){
+    char *postfix;
+    int i = 0, j = 0;
+    int len = strlen(infix);
+    postfix = (char *)malloc((len+1)*sizeof(char));
+    while (infix[i] != '\0')
+    {
+        if(isoperand(infix[i])){
+            postfix[j++] = infix[i++];
+        }
+        else{
+            if(pre(infix[i])>pre(top->data)){
+                push(infix[i++]);
+            }
+            else{
+                postfix[j++] = pop();
+            }
+        }
+        /* code */
+    }
+    while(top != NULL){
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+    return postfix;
+    
+}
+
+
+
+int main()
+{
+    char *infix = "a+b*c-d/e+f";
+    push('#');
+    char *postfix = Convert(infix);
+    printf("%s", postfix);
+    return 0;
+}
