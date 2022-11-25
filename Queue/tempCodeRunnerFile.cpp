40ada3373038c1e9808c1c131ea677f13c0291e5
Ishh91 <ishantsharma9105@gmail.com>
2022-11-25 22:24:05 +0530

    stack

--- Queue/tempCodeRunnerFile.cpp
+++ Queue/tempCodeRunnerFile.cpp
@@ -1,2 +1,14 @@
-cout<<q.top()<<endl;
-    q.pop();
+multiset<int,greater<int>>s;
+    vector<int> ans;
+    for(int i = 0;i<k;i++){
+        s.insert(a[i]);
+    }
+    ans.push_back(*s.begin());
+    for(int i = k;i<n;i++){
+        s.erase(s.lower_bound(a[i-x]));
+        s.insert(a[i]);
+        ans.push_back(*s.begin());
+    }
+    for(auto i: ans){
+        cout<<i<<endl;
+    }
