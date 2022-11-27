d7d05c9ba722e89f2e0edca69e2a41dc6b0fea9c
Ishh91 <ishantsharma9105@gmail.com>
2022-11-27 18:26:21 +0530

    Tree update

--- Tree/queue.h
+++ Tree/queue.h
@@ -1,48 +1,47 @@
-
 struct Node
 {
     struct Node *lchild;
-    struct Node *rchild;
     int data;
-    /* data */
+    struct Node *rchild;
 };
-
 struct Queue
 {
     int size;
     int front;
     int rear;
-    struct **Q;
+    struct Node **Q;
 };
 void create(struct Queue *q, int size)
 {
     q->size = size;
-    q->front = q->rear = -1;
-    q->Q = (Node **)malloc(q->size * sizeof(Node *));
+    q->front = q->rear = 0;
+    q->Q = (struct Node **)malloc(q->size * sizeof(struct
+                                                   Node *));
 }
-void enqueue(struct Queue *q, Node * x)
+void enqueue(struct Queue *q, struct Node *x)
 {
-    if (q->rear == q->size - 1)
+    if ((q->rear + 1) % q->size == q->front)
         printf("Queue is Full");
     else
     {
-        q->rear++;
+        q->rear = (q->rear + 1) % q->size;
         q->Q[q->rear] = x;
     }
 }
-Node * dequeue(struct Queue *q)
+struct Node *dequeue(struct Queue *q)
 {
-    struct x = NULL;
+    struct Node *x = NULL;
 
     if (q->front == q->rear)
         printf("Queue is Empty\n");
     else
     {
-        q->front++;
+        q->front = (q->front + 1) % q->size;
         x = q->Q[q->front];
     }
     return x;
 }
-int isEmpty(struct Queue q){
+int isEmpty(struct Queue q)
+{
     return q.front == q.rear;
 }
